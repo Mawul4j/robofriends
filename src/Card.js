a5f31eecd8a90e0429a9220ff41224c0d13dcfde
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-02-28 09:55:03 -0500

    add card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src='https://robohash.org/kekeli'/>
+            <div>
+                <h2>Kekeli Ameko</h2>
+                <p>kekeli.ameko@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
